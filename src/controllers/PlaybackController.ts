eab50120c57c0ea12e3cc70e52da8530d642c9f9
Mat Sz <contact@matsz.dev>
2020-01-05 20:43:54 +0100

    added playback control

--- src/controllers/PlaybackController.ts
+++ src/controllers/PlaybackController.ts
@@ -1,24 +1,77 @@
-import { Controller, QueryParam, Get, Post } from 'routing-controllers';
+import { Controller, QueryParam, Get, Post, UseBefore, Req } from 'routing-controllers';
+import { AuthenticationMiddleware } from '../middleware/AuthenticationMiddleware';
+import SpotifyWebApi = require('spotify-web-api-node');
 
 @Controller()
+@UseBefore(AuthenticationMiddleware)
 export class PlaybackController {
-    @Get('/auth')
-    index() {
+    @Get('/playback')
+    async index(@Req() request: any) {
+        const spotify: SpotifyWebApi = request.spotify;
+
+        const track = await spotify.getMyCurrentPlayingTrack();
+        let data: any = {};
+
+        const item = track.body.item;
         
+        data.playback = {
+            active: track.body.is_playing,
+        };
+
+        if (track.body.is_playing) {
+            data.playback.progress = track.body.progress_ms;
+            data.playback.duration = item.duration_ms;
+        }
+
+        data.track = {
+            id: item.id,
+            name: item.name,
+            artistName: item.artists.reduce((string, value, i) =>
+                string + (i > 0 ? ", " : "") + value.name
+            , ""),
+            duration: item.duration_ms,
+        };
+
+        data.album = {
+            id: item.album.id,
+            name: item.album.name,
+        };
+
+        if (track.body.context.type === 'playlist') {
+            const playlistId = track.body.context.uri.split(':').slice(-1)[0];
+            const playlist = await spotify.getPlaylist(playlistId, {
+                fields: 'name',
+            });
+
+            data.playlist = {
+                id: playlistId,
+                name: playlist.body.name,
+            };
+        }
+
+        return {
+            success: true,
+            data,
+        };
     }
 
-    @Post('/auth')
-    start() {
-        
+    @Post('/playback/next')
+    async next(@Req() request: any) {
+        const spotify: SpotifyWebApi = request.spotify;
+        await spotify.skipToNext();
+
+        return {
+            success: true,
+        }
     }
 
-    @Post('/auth/state')
-    state(@QueryParam("key") key: string) {
-        
-    }
+    @Post('/playback/previous')
+    async previous(@Req() request: any) {
+        const spotify: SpotifyWebApi = request.spotify;
+        await spotify.skipToNext();
 
-    @Post('/auth/renew')
-    renew() {
-
+        return {
+            success: true,
+        }
     }
 }
